a827b28951f6c849f310292f1c97b8b4d38abafe
palbijewar <pal.bijewar@gmail.com>
2025-02-01 16:46:13 +0530

    added: tmdb implemented for fetch api

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Search from "./components/search";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    authorization: `Bearer ${API_KEY}`,
+  },
+};
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const fetchMovies = async () => {
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS)
+      if(!response.ok){
+        throw new Error("Error fetching movies. Please try again later.")
+      }
+      const data = response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(`error fetching movies: ${error.message}`);
+      setErrorMessage("Error fetching movies. Please try again later. ");
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
   return (
     <main>
@@ -14,9 +44,14 @@ const App = () => {
             Find <span className="text-gradient">Movies</span> You'll Enjoy
             Without The Hastles
           </h1>
-        </header>
 
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+        <section className="all-movies">
+          <h2>All Movies</h2>
+
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        </section>
       </div>
     </main>
   );
