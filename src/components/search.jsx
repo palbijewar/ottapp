64f5f79c575c2ce4a3cb6bab856cee8ae34e281f
palbijewar <pal.bijewar@gmail.com>
2025-02-01 14:47:34 +0530

    added: hero section and search components

---
+++ src/components/search.jsx
@@ -0,0 +1,20 @@
+// eslint-disable-next-line react/prop-types
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="search">
+      <div>
+        <img src="search.svg" alt="search" />
+
+        <input
+          type="text"
+          placeholder="Search through thousands of movies"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+        />
+       
+      </div>
+    </div>
+  );
+};
+
+export default Search;
